6c2e62a4d3835ee50e6f02ad1abcca3321419a02
Sil <s.chavesmata@gmail.com>
2026-02-16 14:04:23 -0600

    Codigo fuente listo

--- Main.cpp
+++ Main.cpp
@@ -5,9 +5,9 @@
 using namespace std;
 
 // Se mantiene el main limpio gracias a la division de capas
-//int main() {
-//	ConsoleApp app; //Toda la logica de la aplicacion esta encapsulada en la clase ConsoleApp
-//    app.run();
-//    return 0;
-//}
+int main() {
+	ConsoleApp app; //Toda la logica de la aplicacion esta encapsulada en la clase ConsoleApp
+    app.run();
+    return 0;
+}
 
